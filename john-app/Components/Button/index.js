940a150d8f4bfa00af1ac7b5124d0819d25c3534
Matt Laurel <alvarez_william@outlook.com>
2021-10-04 12:20:10 -0700

    added buttons, imported ubunut and roboto font

--- john-app/Components/Button/index.js
+++ john-app/Components/Button/index.js
@@ -8,28 +8,35 @@ const ButtonCont = styled.button`
     align-items: center;
     width: ${props=>props.width};
     height: ${props=>props.height};
-    background-color: #FF7A00;
+    background-color: ${props=>props.bgcolor};
     color: #ffffff;
-    border-radius: 1rem;
+    border-radius: ${props=>props.borderradius};
     border: none;
-    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.35))
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.35));
+    font-size: ${props=>props.fontsize};
 `;
 
 const ButtonText = styled.div`
     display: flex;
     color: #ffffff;
-    font-size: 3rem;
+    font-family: 'Roboto', sans-serif;
 `;
 
 const Button = ({
     width = "20rem",
     height = "5rem",
-    text = "Begin"
+    text = "Begin",
+    borderradius = "1rem",
+    bgcolor = "#FF7A00",
+    fontsize = "3rem"
 }) =>{
     return <ButtonCont
         width = {width}
         height = {height}
         text = {text}
+        borderradius = {borderradius}
+        bgcolor = {bgcolor}
+        fontsize = {fontsize}
     >
         <ButtonText>{text}</ButtonText>
     </ButtonCont>
