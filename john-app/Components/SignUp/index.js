7fc837e644f2e77460d61b74e5d9e2cf440d5dd1
Ibjyot Gill <ibjyot17@gmail.com>
2021-10-07 20:31:01 -0700

    new

---
+++ john-app/Components/SignUp/index.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+const SignCont = styled.div`
+    display: flex;
+    flex-direction: column;
+`
--- john-app/pages/forecast.js
+++ john-app/pages/forecast.js
@@ -17,15 +17,15 @@ const CalendarCont_1 = styled.div
 `
   display: flex;
   justify-content: center;
-  gap: 50px;
-  padding:50px;
+  gap: 3rem;
+  padding:3rem;
 `
 const CalendarCont_2 = styled.div
 `
   display: flex;
   justify-content: center;
-  gap: 50px;
-  padding:50px;
+  gap: 3rem;
+  padding:3rem;
 `
 
 export default function Forecast() {
@@ -56,7 +56,7 @@ export default function Forecast() {
         <EventInfoCard 
           h1_day="1"
           toph2_day="Saturday"
-          h1_time="9:30"
+          h1_time="9:30pm"
           h2_eventname="Get Drunk"
           number_of_people="2+ People Involved"
         />
@@ -66,22 +66,22 @@ export default function Forecast() {
         <EventInfoCard 
           h1_day="2"
           toph2_day="Sunday"
-          h1_time="9:30"
-          h2_eventname="Get Drunk"
-          number_of_people="2+ People Involved"
+          h1_time="11:30pm"
+          h2_eventname="Play COD"
+          number_of_people="8+ People Involved"
         />
         <EventInfoCard 
           h1_day="3"
           toph2_day="Monday"
-          h1_time="9:30"
-          h2_eventname="Get Drunk"
-          number_of_people="2+ People Involved"
+          h1_time="9:30am"
+          h2_eventname="Go to Skips Class"
+          number_of_people="15+ People Involved"
         />
         <EventInfoCard 
           h1_day="4"
           toph2_day="Tueday"
-          h1_time="9:30"
-          h2_eventname="Get Drunk"
+          h1_time="11:30"
+          h2_eventname="Ping Pong Time"
           number_of_people="2+ People Involved"
         />
       </CalendarCont_2>
